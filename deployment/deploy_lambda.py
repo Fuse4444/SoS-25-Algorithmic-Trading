c1d1279d9588d8b4df28dc7f6c91fa79486a86ce
Tony Soprano <dmech2006@gmail.com>
2025-07-22 00:54:22 +0530

    Delete deployment directory

--- deployment/deploy_lambda.py
+++
@@ -1,5 +0,0 @@
-# For AWS Lambda Zip Packaging
-# 1. pip install packages to ./package
-# 2. zip -r9 function.zip . (after copying script into package dir)
-
-print("Use AWS Console or CLI to upload zipped handler script and dependencies.")
--- deployment/tradevwap.py
+++
@@ -1,5 +0,0 @@
-# Placeholder for VWAP (requires price-volume history)
-import pandas as pd
-# In practice, collect historical volume + price and do weighted average
-# Here we simulate it with pandas for example purposes
-print("VWAP strategy not fully implemented. Use historical data and calc weighted price avg.")
